b6e8f035b720fb4ec551c629c7e744641344d309
jimdrix93 <javiihv93@gmail.com>
2018-01-15 02:17:13 +0100

    Modifico Greeting con BYE

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -14,5 +14,10 @@ public class GreetingController {
         model.addAttribute("name", name);
         return "greeting";
     }
+    @RequestMapping("/bye")
+    public String bye(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "bye";
+    }
 
 }
